02b71b7d8c14401b88a8bfd2851c2fb1aa961568
AnanyaRavi11 <134755544+AnanyaRavi11@users.noreply.github.com>
2024-06-25 15:55:52 +0530

    Update hm.py

--- hm.py
+++ hm.py
@@ -1,8 +1,1 @@
-class Hotel:
-    def __init__(self, name, rooms):
-        self.name = name
-        self.rooms = rooms
-
-    def check_availability(self, room_number):
-        if room_number in self.rooms:
-            return f"Room {room_number} is available."
+Hotel management
